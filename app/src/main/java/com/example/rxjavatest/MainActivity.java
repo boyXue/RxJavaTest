379b1dce2fa82865f432ba6f9a3edbc69dbccff6
xueqiang <xueqiang@trustmobi.com>
2020-09-03 15:03:52 +0800

    rxjava1.2代码提交

--- app/src/main/java/com/example/rxjavatest/MainActivity.java
+++ app/src/main/java/com/example/rxjavatest/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.rxjavatest;
 
 import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.rxjavatest.databinding.ActivityMainBinding;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import rx.Observable;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -57,65 +73,65 @@ public class MainActivity extends AppCompatActivity {
 //                                binding.textView.append("\n"+s);
 //                            }
 //                        });
-//                if (!img) {
-//                    progressDialog = new ProgressDialog(MainActivity.this);
-//                    progressDialog.setTitle("download run");
-//                    progressDialog.show();
-//                    Observable.just(PATH)
-//                            .map(new Func1<String, Bitmap>() {
-//                                @Override
-//                                public Bitmap call(String s) {
-//                                    try {
-//                                        URL url = new URL(s);
-//                                        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-//                                        httpURLConnection.setConnectTimeout(5000);
-//                                        int responseCode = httpURLConnection.getResponseCode();
-//                                        if (responseCode == HttpURLConnection.HTTP_OK) {
-//                                            InputStream inputStream = httpURLConnection.getInputStream();
-//                                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-//                                            return bitmap;
-//                                        }
-//
-//                                    } catch (IOException e) {
-//                                        e.printStackTrace();
-//                                    }
-//                                    return null;
-//                                }
-//                            })
-//                            .map(new Func1<Bitmap, Bitmap>() {//Fun1是带有返回值的
-//                                @Override
-//                                public Bitmap call(Bitmap bitmap) {
-//                                    Paint paint = new Paint();
-//                                    paint.setTextSize(88);
-//                                    paint.setColor(Color.BLUE);
-//                                    return drawTextToBitmap.drawTextToBitmap(bitmap, "这是水印", paint, 80, 80);
-//                                }
-//                            })
-//                            .subscribeOn(Schedulers.io())//给上面的代码分配到异步线程
-//                            .observeOn(AndroidSchedulers.mainThread())//切换到主线程
-//                            .subscribe(new Observer<Bitmap>() {
-//                                @Override
-//                                public void onCompleted() {
-//
-//                                }
-//
-//                                @Override
-//                                public void onError(Throwable e) {
-//
-//                                }
-//
-//                                @Override
-//                                public void onNext(Bitmap bitmap) {
-//                                    if (progressDialog != null) {
-//                                        progressDialog.dismiss();
-//                                    }
-//                                    binding.imageView.setImageBitmap(bitmap);
-//                                    img=true;
-//                                }
-//                            });
-//                } else {
-//                    Toast.makeText(MainActivity.this, "图片已加载，请不要重复操作", Toast.LENGTH_SHORT).show();
-//                }
+                if (!img) {
+                    progressDialog = new ProgressDialog(MainActivity.this);
+                    progressDialog.setTitle("download run");
+                    progressDialog.show();
+                    Observable.just(PATH)
+                            .map(new Func1<String, Bitmap>() {
+                                @Override
+                                public Bitmap call(String s) {
+                                    try {
+                                        URL url = new URL(s);
+                                        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                                        httpURLConnection.setConnectTimeout(5000);
+                                        int responseCode = httpURLConnection.getResponseCode();
+                                        if (responseCode == HttpURLConnection.HTTP_OK) {
+                                            InputStream inputStream = httpURLConnection.getInputStream();
+                                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                                            return bitmap;
+                                        }
+
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                    return null;
+                                }
+                            })
+                            .map(new Func1<Bitmap, Bitmap>() {//Fun1是带有返回值的
+                                @Override
+                                public Bitmap call(Bitmap bitmap) {
+                                    Paint paint = new Paint();
+                                    paint.setTextSize(88);
+                                    paint.setColor(Color.BLUE);
+                                    return drawTextToBitmap.drawTextToBitmap(bitmap, "这是水印", paint, 80, 80);
+                                }
+                            })
+                            .subscribeOn(Schedulers.io())//给上面的代码分配到异步线程
+                            .observeOn(AndroidSchedulers.mainThread())//切换到主线程
+                            .subscribe(new Observer<Bitmap>() {
+                                @Override
+                                public void onCompleted() {
+
+                                }
+
+                                @Override
+                                public void onError(Throwable e) {
+
+                                }
+
+                                @Override
+                                public void onNext(Bitmap bitmap) {
+                                    if (progressDialog != null) {
+                                        progressDialog.dismiss();
+                                    }
+                                    binding.imageView.setImageBitmap(bitmap);
+                                    img = true;
+                                }
+                            });
+                } else {
+                    Toast.makeText(MainActivity.this, "图片已加载，请不要重复操作", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
